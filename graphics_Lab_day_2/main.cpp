d55280c08e5ae0185df68ad3106288076c71eb62
Mahmudul Hasan <52478411+mahmudul-11@users.noreply.github.com>
2021-02-07 01:32:37 -0800

    Add files via upload

---
+++ graphics_Lab_day_2/main.cpp
@@ -0,0 +1,61 @@
+#include<windows.h>
+#include<GL/glut.h>
+ void display(void)
+ {
+    //Line
+     /*
+    glColor3f(1,0,0);
+    glBegin(GL_LINES);
+    glVertex2f(-0.5,-0.5);
+    glVertex2f(0.5,0.5);
+    glVertex2f(0.5,-0.5);
+    glVertex2f(-0.5,0.5);
+    glEnd();
+    //Point
+    glBegin(GL_POINTS);
+    glVertex2f(0,0.5);
+    glEnd();*/
+
+   /* glColor3f(0,1,0);
+    glBegin(GL_TRIANGLES);
+    glVertex2f(1,0);
+    glVertex2f(0,1);
+    glVertex2f(-1,0);
+    glEnd();
+    glColor3f(0,0.5,0.5);
+    glBegin(GL_QUADS);
+    glVertex2f(-0.5,0);
+     glVertex2f(0.5,0);
+      glVertex2f(0.5,-1);
+
+       glVertex2f(-0.5,-1);
+       glEnd(); */
+
+       //polygon
+       glColor3f(0.7,0.3,0);
+       glBegin(GL_POLYGON);
+       glVertex2f(0,0.7);
+       glVertex2f(0.7,0.3);
+       glVertex2f(0.7,-0.3);
+       glVertex2f(0,-0.7);
+       glVertex2f(-0.7,-0.3);
+       glVertex2f(-0.7,0.3);
+       glEnd();
+
+
+    glFlush();
+
+
+ }
+ int main(int argc,char **argv)
+ {
+     glutInit(&argc, argv);
+     glutInitDisplayMode(GLUT_SINGLE);
+     glutInitWindowSize(700,600);
+     glutInitWindowPosition(150,100);
+     glutCreateWindow("Lab trial");
+     glutDisplayFunc(display);
+     glutMainLoop();
+     return 0;
+
+ }
